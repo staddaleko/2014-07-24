15112b9c2fb7f572a9d93ef3f3d48fd98e5ab042
staddaleko <staddaleko@gmail.com>
2014-07-24 12:54:14 +0200

    1254

---
+++ WpfApplication1doWCF/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WcfService1;
+using System.ServiceModel;
+
+namespace WpfApplication1doWCF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool serviceStarted = false;
+        ServiceHost myServiceHost = null;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (serviceStarted)
+            {
+                myServiceHost.Close();
+                serviceStarted = false;
+                button1.Content = "start service";
+
+            }
+            else
+            {
+                Uri baseAddress = new Uri("net.tcp://localhost:2202/Service1");
+
+                NetTcpBinding binding = new NetTcpBinding();
+
+                myServiceHost = new ServiceHost(typeof(Service1), baseAddress);
+                myServiceHost.AddServiceEndpoint(typeof(IService1), binding, baseAddress);
+
+                myServiceHost.Open();
+
+                serviceStarted = true;
+                button1.Content = "Stop Service";
+            }
+        }
+    }
+}
