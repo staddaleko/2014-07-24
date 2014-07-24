38bf4b60d6a5740da98924c8fcdb31f4605b7c7c
staddaleko <staddaleko@gmail.com>
2014-07-24 11:52:14 +0200

    11:51 brakuje tylko klasy Main - reszta działa

---
+++ WcfService1/IService1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        StudentKlasa GetStudent(int id);
+
+        [OperationContract]
+        int SetStudent(string imie, string nazwisko);
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    
+    [DataContract] //to się definiuje jako pierwsze
+    public class StudentKlasa
+    {
+        //bool boolValue = true;
+        //string stringValue = "Hello ";
+
+        
+        
+        int id_studenta; //mała litera
+        string imie;
+        string nazwisko;
+        
+        [DataMember]
+        public int Id_studenta //publiczne, więc duza litera
+        {
+            get { return id_studenta; }
+            set { id_studenta = value; }
+        }
+
+        [DataMember]
+        public string Imie
+        {
+            get { return imie; }
+            set { imie = value; }
+        }
+
+        [DataMember]
+        public string Nazwisko
+        {
+            get { return nazwisko; }
+            set { nazwisko = value; }
+        }
+
+        public StudentKlasa()
+        { }
+        
+        public StudentKlasa(int _id, string _imie, string _nazwisko)
+        {
+            this.id_studenta = _id;
+            this.imie = _imie;
+            this.nazwisko = _nazwisko;
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private DataClasses1DataContext db = new DataClasses1DataContext();
+
+        public StudentKlasa GetStudent(int id)
+        {
+            var query = (from s in db.StdntTabelas
+                         where s.ID_Stdnt == id
+                         select new { s.ID_Stdnt, s.ImieStdnt, s.NazwiskoStdnt }).ToList();
+
+            return new StudentKlasa(query[0].ID_Stdnt,query[0].ImieStdnt,query[0].NazwiskoStdnt);
+            
+        }
+
+
+        public int SetStudent(string imie, string nazwisko)
+        {
+            StdntTabela S = new StdntTabela();
+            S.ImieStdnt = imie;
+            S.NazwiskoStdnt = nazwisko;
+            db.StdntTabelas.InsertOnSubmit(S);
+            db.SubmitChanges();
+            var query = (from s in db.StdntTabelas
+                         where s.ImieStdnt == imie && s.NazwiskoStdnt == nazwisko
+                         select new { s.ID_Stdnt }).ToList();
+            return query[0].ID_Stdnt;
+        }
+
+
+    }
+}
